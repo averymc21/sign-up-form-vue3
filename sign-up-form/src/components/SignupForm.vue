16b819c849137cd89864f3a0e107f9a78138a296
amclean37 <Avery.McLean@gtri@gatech.edu>
2024-06-21 12:18:30 -0400

    final details

--- sign-up-form/src/components/SignupForm.vue
+++ sign-up-form/src/components/SignupForm.vue
@@ -1,11 +1,13 @@
 <template>
     <div class=" fullform">
-        <form>
+        <form @submit="handleSubmit">
             <label>Email:</label>
             <input type="email" required v-model="email">
 
             <label>Password:</label>
             <input type="password" required v-model="password">
+
+            <div v-if="passwordError" class="error">{{ passwordError }} </div>
 
             <label>Role:</label>
             <select v-model="role">
@@ -16,13 +18,16 @@
             <label>Skills:</label>
             <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
             <div v-for="skill in skills" :key="skill" class="pill"> 
-                {{ skill }}
+                <span @click="deleteSkill(skill)">{{ skill }}</span>
             </div>
 
             <div class="terms">
                 <input type="checkbox"  v-model="terms" required>
                 <label>Accept terms and conditions</label>
 
+            </div>
+            <div class ="submit">
+                <button>Create an Account</button>
             </div>
         </form>
         
@@ -38,7 +43,8 @@ export default {
             role: '',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: ''
             
         }
     },
@@ -50,6 +56,24 @@ export default {
                 
                 }
                 this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill !== item
+            })
+
+        },
+        handleSubmit() {
+            // validate password
+            this.passwordError = this.password.length > 5? 
+            '' : 'password must be at least 6 chars long' 
+            if (!this.passwordError) {
+                console.log('email:', this.email)
+                console.log('password: ', this.password)
+                console.log('role: ', this.skills)
+                console.log('terms accepted: ', this.terms)
+
             }
         }
     }
@@ -104,6 +128,26 @@ input[type="checkbox"]{
     font-weight: bold;
     color: #777;
     cursor: pointer;
+
+}
+button {
+    background: rgb(250, 113, 136);
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+
+
+}
+.submit {
+    text-align: center;
+}
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 
 }
 </style>
